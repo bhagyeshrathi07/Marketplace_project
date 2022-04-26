5197a9dee7a5df9ca8d5fef230d429f73ba8b190
tejaskulkarni7 <tejkul7@gmail.com>
2022-04-26 14:08:51 -0700

    Added HTML and CSS for the profile page, modified routes.py with /profilepage

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,15 @@ from flask import render_template
 @myapp_obj.route("/login")
 def loginPage():
     return render_template('home.html')
+
+
+
+
+
+
+
+
+
+@myapp_obj.route("/profile")
+def profile():
+    return render_template('profilepage.html')
