20d59e3072ee3df2ae66c75b0eb672780273422c
Winson Ma <winson.ma@sjsu.edu>
2022-04-26 15:53:19 -0700

    implemented register route in routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,22 @@ from flask import render_template
 @myapp_obj.route("/login")
 def loginPage():
     return render_template('home.html')
-  
+
 @myapp_obj.route("/create")
 def createAccount():
-	return render_template('createAcc.html')
+	register = RegistrationForm()
+
+	if registration.validate_on_submit():
+		username = register.username.data
+		password = register.password.data
+
+		user = User(username=username, password=password)
+		db.session.add(user)
+		db.session.commit()
+
+		return
+
+	return render_template('createAcc.html', form=register)
 
 @myapp_obj.route("/profile")
 def profile():
