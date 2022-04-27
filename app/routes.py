0981b424a3f90a891c6f207065c046810a5c7224
BhagyeshRathi <Bhageyesh2161@gmail.com>
2022-04-26 22:44:43 -0700

    added route for market.html

--- app/routes.py
+++ app/routes.py
@@ -11,16 +11,25 @@ name = "Tejas"
 def home():
 	return render_template('home.html')
 
+@myapp_obj.route("/market") #, methods=('GET', 'POST'))
+def market():
+	#sample Items in market
+	items = [
+		{'id': 1, 'name': 'iphone7', 'barcode': '893212299897', 'price': 500}
+		{'id': 2, 'name': 'macbook air', 'barcode': '985792304857', 'price': 1500}
+		{'id': 3, 'name': 'Ipad pro', 'barcode': '856342895798', 'price': 899}
+	]
+	return render_template('market.html', items = items)
+
 @myapp_obj.route("/login", methods=('GET', 'POST'))
 def loginPage():
-    return render_template('home.html')
+    return render_template('login.html')
   
-@myapp_obj.route("/create", methods=('GET', 'POST'))
-
+@myapp_obj.route("/signup", methods=('GET', 'POST'))
 def createAccount():
 	register = RegistrationForm()
 
-	if registration.validate_on_submit():
+	"""if registration.validate_on_submit():
 		username = register.username.data
 		password = register.password.data
 
@@ -28,8 +37,7 @@ def createAccount():
 		db.session.add(user)
 		db.session.commit()
 
-		return
-
+		return"""
 	return render_template('createAcc.html', form=register)
 
 @myapp_obj.route("/profile", methods=('GET', 'POST'))
