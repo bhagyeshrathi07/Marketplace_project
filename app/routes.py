e3c7fed990223c844865d973c7a7320b205c8c1a
BhagyeshRathi <Bhageyesh2161@gmail.com>
2022-04-28 14:52:54 -0700

    removed unused imports and solved flask errors everything working as expected

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,15 @@
-from flask_login import login_url
+from flask import render_template
 from app import myapp_obj
-from flask import render_template, Flask, flash, redirect
-from wtforms import StringField, SubmitField, PasswordField, BooleanField
-from wtforms.validators import DataRequired
-from flask_wtf import FlaskForm
 
 name = "Tejas"
 
-@myapp_obj.route("/", methods=('GET', 'POST'))
+@myapp_obj.route("/")#, methods=('GET', 'POST'))
 def home():
-	return render_template('home.html')
+	return render_template('home.html', title='Home')
+
+@myapp_obj.route("/login.html")#, methods=('GET', 'POST'))
+def loginPage():
+    return render_template('login.html', title='Login')
 
 @myapp_obj.route("/market.html") #, methods=('GET', 'POST'))
 def market():
@@ -17,15 +17,11 @@ def market():
         {'name': 'iphone 12 Pro', 'description': '256GB', 'price': 1000},
 		{'name': 'macbook pro', 'description': '512 GB, space gray', 'price': 1600},
         {'name': 'ipad pro', 'description': '256 GB WiFi', 'price': 850}]
-    return render_template('market.html', items=items)
-
-@myapp_obj.route("/login.html", methods=('GET', 'POST'))
-def loginPage():
-    return render_template('login.html')
+    return render_template('market.html', items=items, title='Market')
   
-'''@myapp_obj.route("/createAcc.html", methods=('GET', 'POST'))
+@myapp_obj.route("/createAcc.html", methods=('GET', 'POST'))
 def createAccount():
-	register = RegistrationForm()
+	'''register = RegistrationForm()
 
 	if registration.validate_on_submit():
 		username = register.username.data
@@ -35,13 +31,14 @@ def createAccount():
 		db.session.add(user)
 		db.session.commit()
 
-		return
-	return render_template('createAcc.html', form=register)'''
+		return'''
+	return render_template('createAcc.html', title='Signup')
 
 @myapp_obj.route("/profilepage.html", methods=('GET', 'POST'))
 def profile():
-    return render_template('profilepage.html', name = name)
+    return render_template('profilepage.html', name = name, title='My Profile')
 
-@myapp_obj.route("/cart.html", methods=('GET', 'POST'))
+@myapp_obj.route("/cart.html")#, methods=('GET', 'POST'))
 def cart():
-    return render_template('cart.html')
+    return render_template('cart.html', title= 'Cart')
+
