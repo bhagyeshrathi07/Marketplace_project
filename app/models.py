580ae22d8a3fceefddebc768f366580591564a22
BhagyeshRathi <Bhageyesh2161@gmail.com>
2022-04-29 13:50:26 -0700

    solved database error, defined relationship between user and item, updated class user and item with relationships and budget

--- app/models.py
+++ app/models.py
@@ -1,18 +1,24 @@
-from __init__ import db
+from app import db
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), nullable=False, index=True, unique=True)
-	password_hash = db.Column(db.String(128), nullable=False)
+	username = db.Column(db.String(length=30), nullable=False, unique=True)
+	email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+	password_hash = db.Column(db.String(length=60), nullable=False)
+	budget = db.Column(db.Integer(), nullable=False, default=2000)
+	items = db.relationship('Item', backref='owned_user', lazy=True)
 
 	def __repr__(self):
 		return f'User {self.username}'
 
+
+
 class Item(db.Model):
 	id = db.Column(db.Integer(), primary_key=True)
-	name = db.Column(db.String(64), nullable=False, unique=True)
+	name = db.Column(db.String(length=30), nullable=False, unique=True)
 	price = db.Column(db.Integer(), nullable=False)
-	description = db.Column(db.String(1024), nullable=False, unique=True)
+	description = db.Column(db.String(length=1024), nullable=False, unique=True)
+	owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
 	def __repr__(self):
     		return f'Item {self.name}'
