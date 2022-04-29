914e9871e384ab14be82ed0a7e18c4ba6cb894c1
BhagyeshRathi <Bhageyesh2161@gmail.com>
2022-04-29 16:32:44 -0700

    connected forms with flask forms

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,11 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 myapp_obj = Flask(__name__);
-
 db = SQLAlchemy(myapp_obj);
+
+
 myapp_obj.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-#myapp_obj.config.from_mapping(SECRET_KEY = 'hr738rgh3q2[qp[2dq//2dqjd9q8/2jd2dm2d')
+myapp_obj.config['SECRET_KEY'] = '032acac8f265a4205baaa601'      #DO NOT CHANGE THIS HEXADECIMAL SECRET_KEY
 
 from app import routes
