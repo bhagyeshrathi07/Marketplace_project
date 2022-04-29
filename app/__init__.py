97e25a95c38cd8230288bf85ac3dba1df485cd2f
BhagyeshRathi <Bhageyesh2161@gmail.com>
2022-04-29 13:48:46 -0700

    solved the error 404 not found

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 myapp_obj = Flask(__name__);
-db = SQLAlchemy(myapp_obj);
 
+db = SQLAlchemy(myapp_obj);
 myapp_obj.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 #myapp_obj.config.from_mapping(SECRET_KEY = 'hr738rgh3q2[qp[2dq//2dqjd9q8/2jd2dm2d')
 
-
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,19 @@
 
 from app.forms import *
 from flask import render_template
-from app import myapp_obj
-from models import Item, User
+from app import myapp_obj, db
+from app.models import Item, User
 
 
-@myapp_obj.route("/")#, methods=('GET', 'POST'))
+@myapp_obj.route("/", methods=('GET', 'POST'))
 def home():
 	return render_template('home.html', title='Home')
 
-@myapp_obj.route("/login.html")#, methods=('GET', 'POST'))
+@myapp_obj.route("/login", methods=('GET', 'POST'))
 def loginPage():
     return render_template('login.html', title='Login')
 
-@myapp_obj.route("/market") #, methods=('GET', 'POST'))
+@myapp_obj.route("/market", methods=('GET', 'POST'))
 def market():
     items = Item.query.all()
     return render_template('market.html', items=items, title='Market')
@@ -37,7 +37,7 @@ def signupPage():
 def profile():
     return render_template('profilepage.html', title='My Profile')
 
-@myapp_obj.route("/cart")#, methods=('GET', 'POST'))
+@myapp_obj.route("/cart", methods=('GET', 'POST'))
 def cart():
     return render_template('cart.html', title= 'Cart')
 
