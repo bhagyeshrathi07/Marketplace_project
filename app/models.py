cbb832fdcf069b70162576059f73021f34b7be7b
BhagyeshRathi <Bhageyesh2161@gmail.com>
2022-04-29 19:18:35 -0700

    connected signup form and page with database

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 
 class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
+	id = db.Column(db.Integer(), primary_key=True)
 	username = db.Column(db.String(length=30), nullable=False, unique=True)
 	email_address = db.Column(db.String(length=50), nullable=False, unique=True)
 	password_hash = db.Column(db.String(length=60), nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 
 from app.forms import RegistrationForm
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import myapp_obj, db
 from app.models import Item, User
 
@@ -18,9 +18,14 @@ def market():
     items = Item.query.all()
     return render_template('market.html', items=items, title='Market')
   
-@myapp_obj.route("/signup", methods=('GET', 'POST'))
+@myapp_obj.route("/signup", methods=['GET', 'POST'])
 def signupPage():
 	form = RegistrationForm()
+	if form.validate_on_submit():
+		user_to_create = User(username=form.username.data, email_address=form.email_address.data, password_hash=form.password1.data)
+		db.session.add(user_to_create)
+		db.session.commit()
+		return redirect(url_for('market'))	
 	return render_template('signup.html', form=form, title='Signup')
 
 @myapp_obj.route("/profilepage", methods=('GET', 'POST'))
