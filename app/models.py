b36da037c80afdbe08deb3e3cdb631c10a51714f
BhagyeshRathi <Bhageyesh2161@gmail.com>
2022-05-01 13:09:52 -0700

    minor changes

--- app/models.py
+++ app/models.py
@@ -16,7 +16,7 @@ class User(db.Model, UserMixin):
 	@property
 	def prettier_budget(self):
 		if len(str(self.budget)) >= 4:
-			return f'${str(self.budget)[:-3]},{str(self.budget)[-3:]}'
+			return f'$ {str(self.budget)[:-3]},{str(self.budget)[-3:]}'
 		else:
 			return f"${self.budget}"
 
