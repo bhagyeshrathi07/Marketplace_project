1afb1f828e4b11b52a80f4de202623882d89b152
BhagyeshRathi <Bhageyesh2161@gmail.com>
2022-05-15 23:22:14 -0700

    increased budget

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(db.Model, UserMixin):
 	username = db.Column(db.String(length=30), nullable=False, unique=True)
 	email_address = db.Column(db.String(length=50), nullable=False, unique=True)
 	password_hash = db.Column(db.String(length=60), nullable=False)
-	budget = db.Column(db.Integer(), nullable=False, default=2000)
+	budget = db.Column(db.Integer(), nullable=False, default= 60000)
 	items = db.relationship('Item', backref='owned_user', lazy=True)
 
 	@property
